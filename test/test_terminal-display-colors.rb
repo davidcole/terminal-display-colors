fae38f6d5f59dccf154f6677e92c0f5029a484aa
David Cole <david.cole@digitalcharleston.com>
2014-04-17 10:44:32 -0400

    Cleanup tests. Bump version.

--- test/test_terminal-display-colors.rb
+++ test/test_terminal-display-colors.rb
@@ -1,4 +1,9 @@
-require File.dirname(__FILE__) + '/test_helper.rb'
+#!/usr/bin/env ruby -w
+
+$:.unshift File.join( File.dirname( __FILE__ ), '..', 'lib' )
+
+require 'test/unit'
+require 'terminal-display-colors'
 
 class TestTerminalDispayColors < Test::Unit::TestCase
 
